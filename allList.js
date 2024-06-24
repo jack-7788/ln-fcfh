f127f93e3c3ad3f4029a5a04076bcf2006971dfa
wangkang <596288535@qq.com>
2024-06-24 21:47:26 +0800

    i

--- allList.js
+++ allList.js
@@ -1,49 +1,62 @@
 const allList = [
-  {
-      buildNum: 1,
-      position: [116.404804, 39.909125, 49.58],
-      detail: [
-          {
-              layers: 1,
-              attribute: {
-                  height: 101,
-                  width: 101,
-              },
-          },
-          {
-              layers: 2,
-              attribute: {
-                  height: 102,
-                  width: 102,
-              },
-          },
-          {
-              layers: 3,
-              attribute: {
-                  height: 103,
-                  width: 103,
-              },
-          },
-      ],
-  },
-  {
-      buildNum: 2,
-      position: [116.405242, 39.909118, 49.58],
-      detail: [
-          {
-              layers: 1,
-              attribute: {
-                  height: 201,
-                  width: 201,
-              },
-          },
-          {
-              layers: 2,
-              attribute: {
-                  height: 202,
-                  width: 202,
-              },
-          },
-      ],
-  },
+    {
+        buildNum: 1,
+        position: [116.404804, 39.909125, 49.58],
+        detail: [
+            {
+                layers: 1,
+                attribute: {
+                    height: 101,
+                    width: 101,
+                    name: 'zs',
+                    rent:true,
+                },
+            },
+            {
+                layers: 2,
+                attribute: {
+                    height: 102,
+                    width: 102,
+                    name: 'ls',
+                    rent:false,
+
+                },
+            },
+            {
+                layers: 3,
+                attribute: {
+                    height: 103,
+                    width: 103,
+                    name: 'ww',
+                    rent:true,
+                },
+            },
+        ],
+    },
+    {
+        buildNum: 2,
+        position: [116.405242, 39.909118, 49.58],
+        detail: [
+            {
+                layers: 1,
+                attribute: {
+                    height: 201,
+                    width: 201,
+                    name: 'zl',
+                    rent:false,
+
+                },
+            },
+            {
+                layers: 2,
+                attribute: {
+                    height: 202,
+                    width: 202,
+                    name: 't7',
+                    rent:true,
+
+                },
+            },
+        ],
+    },
 ]
