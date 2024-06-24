f6febfce915b75782787fcc48052f37652a09b3f
wangkang <70838383+jack-7788@users.noreply.github.com>
2024-06-24 19:39:03 +0800

    fix: -

---
+++ allList.js
@@ -0,0 +1,49 @@
+const allList = [
+  {
+      buildNum: 1,
+      position: [116.404804, 39.909125, 49.58],
+      detail: [
+          {
+              layers: 1,
+              attribute: {
+                  height: 101,
+                  width: 101,
+              },
+          },
+          {
+              layers: 2,
+              attribute: {
+                  height: 102,
+                  width: 102,
+              },
+          },
+          {
+              layers: 3,
+              attribute: {
+                  height: 103,
+                  width: 103,
+              },
+          },
+      ],
+  },
+  {
+      buildNum: 2,
+      position: [116.405242, 39.909118, 49.58],
+      detail: [
+          {
+              layers: 1,
+              attribute: {
+                  height: 201,
+                  width: 201,
+              },
+          },
+          {
+              layers: 2,
+              attribute: {
+                  height: 202,
+                  width: 202,
+              },
+          },
+      ],
+  },
+]
